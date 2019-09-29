8f35245fa8f99078334b4c6f940a3495dbb69d36
刘超 <tjymmm@gmail.com>
2019-09-29 17:01:30 +0800

    前台后台页面渲染完毕，路由成功指出

--- admin/__init__.py
+++
@@ -1,5 +0,0 @@
-#-*- coding:utf-8 -*-
-# author:刘超
-# datetime:2019/9/26 20:15
-# software: PyCharm
-# TEL:17854169557
--- app/views.py
+++ app/views.py
@@ -8,4 +8,40 @@ from flask import render_template
 
 @app.route('/')
 def index():
+    '''
+    首页
+    :return:
+    '''
     return render_template('index.html')
+
+@app.route('/admin')
+def admin_index():
+    '''
+    后台管理首页
+    :return:
+    '''
+    return render_template('admin_index.html')
+
+@app.route('/admin_article')
+def admin_article():
+    '''
+    后台管理文章页
+    :return:
+    '''
+    return render_template('admin_article.html')
+
+@app.route('/category')
+def category():
+    '''
+    文章分类页
+    :return:
+    '''
+    return render_template('category.html')
+
+@app.route('/user')
+def user():
+    '''
+    用户页
+    :return:
+    '''
+    return render_template('user.html')
